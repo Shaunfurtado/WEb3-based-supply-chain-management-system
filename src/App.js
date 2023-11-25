2c21b9d3a0f37a1dbed77e4d24fccb121b8f8050
Shaunfurtado <shaunf1801@gmail.com>
2023-11-25 13:58:49 +0530

    App State and card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+const Card = ({ title, description, buttonText }) => (
+  <div className="card w-96 bg-primary text-primary-content">
+    <div className="card-body">
+      <h2 className="card-title">{title}</h2>
+      <p>{description}</p>
+      <div className="card-actions justify-end">
+        <button className="btn">{buttonText}</button>
+      </div>
+    </div>
+  </div>
+);
 
 export default Card;
