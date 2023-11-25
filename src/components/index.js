ea4c671298c4c79982be0b8bae3f8e715cf13a03
Shaunfurtado <shaunf1801@gmail.com>
2023-11-25 14:35:08 +0530

    small change

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import StartShipment from './StartShipment';
 import Footer from './Footer';
 import GetShipment from './GetShipment';
 import Home from './Home';
-import Navbar from './Navbar';
+import NavBar from './NavBar';
 import Table from './Table';
 import TrackingLogs from './TrackingLogs';
 import VerifyProduct from './VerifyProduct';
@@ -19,7 +19,7 @@ export {
     Footer,
     GetShipment,
     Home,
-    Navbar,
+    NavBar,
     Table,
     TrackingLogs,
     StartShipment,
